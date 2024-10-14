49e56ea3085f87d87d7ddaca1498ed17811d1d80
Anthony Alam <anthony.alam2005@gmail.com>
2024-10-14 12:33:54 -0400

    Constructor works fine. On to analysis

--- Backend/CSVAnalyser.py
+++ Backend/CSVAnalyser.py
@@ -12,54 +12,49 @@ for row in csv_reader:
     #do stuff
 '''
 class CSVAnalyser():
-    def __init__(self, associated_file=""):
+    def __init__(self, associated_file="") -> None:
         self.associated_file = associated_file
-        self.acessibility_criteria = []
+        self.accessibility_criteria = []
+        
+        #generate the list of the elements not part of the accessibility rating
+        list_of_non_fields = ["Id", "Name", "Mime type", "Score", "Deleted at", "Library reference", "Url", "Checked on"]
         first_row = ""
 
+        self.list_of_non_fields = list_of_non_fields
+
         #open the csv file
-        print(os.getcwd())
-        print(os.path.abspath(self.associated_file))
         try:
-            with open(self.associated_file, encoding='utf-8') as file:
+            with open("Backend/" + self.associated_file, encoding='utf-8') as file:
                 csv_reader = csv.reader(file, delimiter="\n")
                 first_row = next(csv_reader)
         except FileNotFoundError:
-            print(f"File '{self.associated_file}' not found.")
             return self.try_to_read_csv(False)
-        except Exception as e:
-            print(f"An unexpected error occurred: {e}")
-            return self.try_to_read_csv(False)
+        except Exception:
+            return self.try_to_read_csv(True)
 
 
         #generate a list of the individual element of the first row
-        list_first_row = first_row.split(",")
+        list_first_row = first_row[0].split(",")
 
-        #generate the list of the elements not part of the accessibility rating
-        list_of_non_fields = ["name", "mime type", "score", "deleted at", "library reference", "url", "checked on"]
-
-        count = 0
+        #iterate through the fields specified on the first row of the CSV export
         for i in range(len(list_first_row)):
             #if an accessibility criteria is found, set it's corresponding value
             if list_first_row[i] not in list_of_non_fields:
-                self.acessibility_criteria[count] = list_first_row[i]
-                count += 1
+                self.accessibility_criteria.append(list_first_row[i])
 
-    def change_associated_file(self, new_file):
+    #allows us to change the file associate with the CSVAnalyzer if neccessary
+    def change_associated_file(self, new_file) -> None:
         self.associated_file = new_file
 
-    def try_to_read_csv(self, constructor_worked):
+    #error handling for unexpected behaviour when opening the CSV file provided
+    def try_to_read_csv(self, constructor_worked) -> Exception:
         if (not constructor_worked):
-            return "Error: could not open csv file."
+            return Exception("Error: could not find csv file.")
 
         try:
            # Attempt to read the CSV file
             file_reader = pd.read_csv(self.associated_file)
             return file_reader
-        
-        except FileNotFoundError:
-            # Handle the case where the file does not exist
-            return "Error: CSV file doesn't exist"
     
         except pd.errors.EmptyDataError:
             # Handle the case where the file is empty
@@ -74,6 +69,7 @@ class CSVAnalyser():
             return "Error: Unexpected error occurred"
         
 
+    #analysis algorithm that will evaluate every component of the CSV file
     def run_analysis(self):
         file_reader = self.try_to_read_csv()
         if (type(file_reader) == str):
